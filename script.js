018b972a504fc1b2ee0db389a1d870ca1e476682
aezi <30462422+minichest@users.noreply.github.com>
2021-08-22 18:58:44 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function play() {
+  var audio = document.getElementById("audio");
+  audio.play();
+}
+      
+function modal() {
+  alert("You got played dog, now move off this site!");
+}
+      
+function hack() {
+  play();
+  modal();
+}
