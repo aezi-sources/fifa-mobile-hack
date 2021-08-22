77a66f9c9d6592dcffd560792663192e0cc205aa
aezi <30462422+minichest@users.noreply.github.com>
2021-08-22 19:02:58 +0100

    Update hack.js

--- hack.js
+++ hack.js
@@ -2,12 +2,3 @@ function play() {
   var audio = document.getElementById("audio");
   audio.play();
 }
-      
-function modal() {
-  alert("You got played dog, now move off this site!");
-}
-      
-function hack() {
-  play();
-  modal();
-}
